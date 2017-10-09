2e890b0710d34a966cd8af8b202e40f0530814ad
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-10-08 20:48:04 -0500

    create user and session controllers, user model and add routes to config

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
 
   root "posts#index"
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   get '/about', to: 'pages#about'
 end
