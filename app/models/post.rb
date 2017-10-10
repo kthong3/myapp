c1f9959e4cb74ef16a8a4aea7b3a99fc63e848fa
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-10-10 07:47:17 -0500

    create models and migrations for tagging and tag, update post form to include tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,25 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: :User
   has_many :comments, dependent: :destroy
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, :author_id, presence: true
+
+  attr_accessor :tag_names
+  after_save :assign_tags
+
+  def tag_names
+    @tag_names || tags.map(&:name).join(' ')
+  end
+
+  private
+  def assign_tags
+    if @tag_names
+      self.tags = @tag_names.split(/\s+/).map do |name|
+        Tag.find_or_create_by_name(name)
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010121910) do
+ActiveRecord::Schema.define(version: 20171010123851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20171010121910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
